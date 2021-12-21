522879e7da110ecf2e841998b197b34062c54b29
Pierre Neidhardt <mail@ambrevar.xyz>
2021-12-21 15:30:30 +0100

    package: Explicitly depend on named-readtables.

--- package.lisp
+++ package.lisp
@@ -3,6 +3,7 @@
   (:documentation "File object finder.
 This is the meta package which includes all others.")
   (:import-from #:trivial-package-local-nicknames)
+  (:import-from #:named-readtables)
   (:use-reexport
    #:fof/file
    #:fof/predicates))
