08540842dab2ee6dc0f27b0676e1cd9825cec88c
vindarel <vindarel@mailz.org>
2024-04-23 17:02:49 +0200

    predicates: rm user, group, kind, executable

--- predicates.lisp
+++ predicates.lisp
@@ -35,45 +35,45 @@
     (some (sera:equals (extension file))
           (cons extension more-extensions))))
 
-(export-always 'kind=)
-(-> kind= (file-kind &rest file-kind) function)
-(defun kind= (kind &rest more-kinds)
-  "Return a predicate for files that match one of the provided `file-kind's."
-  (lambda (file)
-    (some (sera:eqs (kind file))
-          (cons kind more-kinds))))
+;; (export-always 'kind=)
+;; (-> kind= (file-kind &rest file-kind) function)
+;; (defun kind= (kind &rest more-kinds)
+;;   "Return a predicate for files that match one of the provided `file-kind's."
+;;   (lambda (file)
+;;     (some (sera:eqs (kind file))
+;;           (cons kind more-kinds))))
 
-(deftype user-specifier ()
-  "A `string' identifies the user name, a `fixnum' the user ID."
-  `(or string fixnum))
+;; (deftype user-specifier ()
+;;   "A `string' identifies the user name, a `fixnum' the user ID."
+;;   `(or string fixnum))
 
-(export-always 'user=)
-(-> user= (user-specifier &rest user-specifier) function)
-(defun user= (user &rest more-users)
-  "Return a predicate for files that match one of the provided `user-specifier's."
-  (lambda (file)
-    (let ((user-name (user file)))
-      (some (lambda (user)
-              (typecase user
-                (string (string= user-name user))
-                (number (= user (user-id file)))))
-            (cons user more-users)))))
+;; (export-always 'user=)
+;; (-> user= (user-specifier &rest user-specifier) function)
+;; (defun user= (user &rest more-users)
+;;   "Return a predicate for files that match one of the provided `user-specifier's."
+;;   (lambda (file)
+;;     (let ((user-name (user file)))
+;;       (some (lambda (user)
+;;               (typecase user
+;;                 (string (string= user-name user))
+;;                 (number (= user (user-id file)))))
+;;             (cons user more-users)))))
 
-(deftype group-specifier ()
-  "A `string' identifies the group name, a `fixnum' the group ID."
-  `(or string fixnum))
+;; (deftype group-specifier ()
+;;   "A `string' identifies the group name, a `fixnum' the group ID."
+;;   `(or string fixnum))
 
-(export-always 'group=)
-(-> group= (group-specifier &rest group-specifier) function)
-(defun group= (group &rest more-groups)
-  "Return a predicate for files that match one of the provided `group-specifier's."
-  (lambda (file)
-    (let ((group-name (group file)))
-      (some (lambda (group)
-              (typecase group
-                (string (string= group-name group))
-                (number (= group (group-id file)))))
-            (cons group more-groups)))))
+;; (export-always 'group=)
+;; (-> group= (group-specifier &rest group-specifier) function)
+;; (defun group= (group &rest more-groups)
+;;   "Return a predicate for files that match one of the provided `group-specifier's."
+;;   (lambda (file)
+;;     (let ((group-name (group file)))
+;;       (some (lambda (group)
+;;               (typecase group
+;;                 (string (string= group-name group))
+;;                 (number (= group (group-id file)))))
+;;             (cons group more-groups)))))
 
 
 (export-always 'path~)
@@ -127,16 +127,16 @@ file basename."
 of ROOT less deep than LEVEL."
   (apply #'fof/file::match-depth< level root))
 
-(export-always 'executable?)
-(defun executable? (file)
-  "Return T when this file has executable permissions.
+;; (export-always 'executable?)
+;; (defun executable? (file)
+;;   "Return T when this file has executable permissions.
 
-Example:
+;; Example:
 
-(fof:finder* :predicates (list #'fof/p:executable?))"
-  (intersection
-   (permissions file)
-   '(:user-exec :group-exec :other-exec)))
+;; (fof:finder* :predicates (list #'fof/p:executable?))"
+;;   (intersection
+;;    (permissions file)
+;;    '(:user-exec :group-exec :other-exec)))
 
 (export-always 'hidden?)
 (defun hidden? (file)
